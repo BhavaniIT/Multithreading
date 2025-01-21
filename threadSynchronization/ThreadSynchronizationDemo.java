dab9a339dd49f16e60a39931292cd0c6d94e8161
Bhavani IT <bhavanitechit@gmail.com>
2025-01-21 19:59:17 +0530

    Synchronization Examples

--- threadSynchronization/ThreadSynchronizationDemo.java
+++ threadSynchronization/ThreadSynchronizationDemo.java
@@ -1,18 +1,38 @@
 public class ThreadSynchronizationDemo {
-    private static int counter = 0;
+    private static int counter1 = 0;
+    private static int counter2 = 0;
 
     /* Reason for wrong output
      * -> Non atomic operation
      * 1. Load
      * 2. Increment
      * 3. Set 
+     * Called as race condition
+     * 2 threads - same resource
      */
+
+     private static synchronized void increment1(){
+        counter1++;
+     }
+
+     private static synchronized void increment2(){
+        counter2++;
+     }
+
+     /* Output will be correct
+      * But,
+      * Even though t2 has nothing to do with t1
+      * method level synchronization will lock the class
+      * so T2 will have to wait
+      * Issue solved using : Lock with custom object
+      */
+    
     public static void main(String[] args) {
         Thread one = new Thread(()->{
-            for(int i = 0; i < 100000; ++i) counter++;
+            for(int i = 0; i < 100000; ++i) increment1();
         });
         Thread two = new Thread(()->{
-            for(int i = 0; i < 100000; ++i) counter++;
+            for(int i = 0; i < 100000; ++i) increment2();
         });
         
         one.start();
@@ -24,6 +44,6 @@ public class ThreadSynchronizationDemo {
         } catch (InterruptedException e) {
             throw new RuntimeException();
         }
-        System.out.println(counter);
+        System.out.println(counter1+ "**"+counter2);
     }
 }
